12d176223862d58cbce79755facee2304457e1fc
Julian M. Rice <rice.m.julian@gmail.com>
2019-11-15 12:51:57 -0800

    Enrollment working, lookup, unwind, matching

--- application/routes.py
+++ application/routes.py
@@ -73,10 +73,45 @@ def enrollment():
             flash(f"You already registered for {courseTitle}!", "danger")
             return redirect(url_for("courses"))
         else:
-            Enrollment(user_id=user_id, courseID=courseID)
+            Enrollment(user_id=user_id, courseID=courseID).save()
             flash(f"You have enrolled in {courseTitle}!", "success")
 
-    classes = None
+    classes = list(User.objects.aggregate(*[
+            {
+                '$lookup': {
+                    'from': 'enrollment', 
+                    'localField': 'user_id', 
+                    'foreignField': 'user_id', 
+                    'as': 'r1'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$r1', 
+                    'includeArrayIndex': 'r1_id', 
+                    'preserveNullAndEmptyArrays': False
+                }
+            }, {
+                '$lookup': {
+                    'from': 'course', 
+                    'localField': 'r1.courseID', 
+                    'foreignField': 'courseID', 
+                    'as': 'r2'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$r2', 
+                    'preserveNullAndEmptyArrays': False
+                }
+            }, {
+                '$match': {
+                    'user_id': user_id
+                }
+            }, {
+                '$sort': {
+                    'courseID': 1
+                }
+            }
+        ]))
     term    = request.form.get('term')
     return render_template("enrollment.html", enrollment=True, classes=classes, title="Enrollment")
 
